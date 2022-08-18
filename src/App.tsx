ffa34c21097ca5d38d844060d5748e402df4c89e
shu <78002112+shu-a@users.noreply.github.com>
2022-08-18 13:54:10 +0900

    Suspense 추가(Loading 임시)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import './App.css';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ManualIndex from './pages/Manual/Index';
-import HomeIndex from './pages/Home/Index';
-import AboutIndex from './pages/About/Index';
-import NotFound from './NotFound';
-
+// import ManualIndex from './pages/Manual/Index';
+// import HomeIndex from './pages/Home/Index';
+// import AboutIndex from './pages/About/Index';
+// import NotFound from './NotFound';
+const ManualIndex = lazy(() => import('./pages/Manual/Index'));
+const HomeIndex = lazy(() => import('./pages/Home/Index'));
+const AboutIndex = lazy(() => import('./pages/About/Index'));
+const NotFound = lazy(() => import('./NotFound'));
+const renderLoader = () => <p>Loading...</p>;
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<HomeIndex />} />
-        <Route path='Manual' element={<ManualIndex />} />
-        <Route path='About' element={<AboutIndex />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<Suspense fallback={renderLoader()}><HomeIndex /></Suspense>} />
+        <Route path='Manual' element={<Suspense fallback={renderLoader()}><ManualIndex /></Suspense>} />
+        <Route path='About' element={<Suspense fallback={renderLoader()}><AboutIndex /></Suspense>} />
+        <Route path="*" element={<Suspense fallback={renderLoader()}><NotFound /></Suspense>} />
       </Routes>
     </div>
   );
